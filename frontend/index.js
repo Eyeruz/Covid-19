5f7e5e7bbdbbfc5ee2b1152d13a489da5e97e821
Eyeruz <3yerus@gmail.com>
2020-09-15 11:26:42 -0400

    All cases displayed

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,19 @@
 
 const btn = () => document.getElementById('btn')
-const 
+const totalR = () => document.getElementById('totalR')
+const totalD = () => document.getElementById('totalD')
+const totalC = () => document.getElementById('totalC')
+const h3 = () => document.getElementById('allCases')
 
 document.addEventListener("DOMContentLoaded", function(){
  onCall()
  myApi()
+ myApi2()
 })
 
 function onCall() {
 btn().addEventListener("click", changeColor) 
+h3().
 }
 
 
@@ -27,15 +32,57 @@ function myApi() {
 	}
 })
 .then(res => res.json())
-.then(response => {
-	console.log(response);
+.then(value=> {
+    allCases(value);
 })
 .catch(err => {
 	console.log(err);
 });
 }
 
-function allCases(){
+function allCases(value){
+    
+const val = value.response.reduce((acc, val) => {
+    return val.cases.total + acc
+   }, 0)
+   addCases(val)
+}
 
+
+function addCases(value) {
+h3().innerText = value
 
 }
+
+fuction
+
+
+function myApi2() {
+    fetch("https://covid-193.p.rapidapi.com/statistics", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-193.p.rapidapi.com",
+		"x-rapidapi-key": "9d691e50d2msh4aa205c2ff027f5p10f338jsn0e4e7c874dc3"
+	}
+})
+.then(res => res.json())
+.then(value=> {
+    allDeaths(value);
+})
+.catch(err => {
+	console.log(err);
+});
+}
+
+function allDeaths(value){
+    debugger;
+
+    const val = value.response.reduce((acc, val) => {
+        return val.deaths.total + acc
+       }, 0)
+       
+
+    }
+
+
+
