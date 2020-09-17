90da6f913f2e84f9957974cb4c6d7afeead923d6
Eyeruz <3yerus@gmail.com>
2020-09-16 21:49:14 -0400

    rails api GET

--- frontend/index.js
+++ frontend/index.js
@@ -9,6 +9,7 @@ const h33 = () => document.getElementById('allRecovered')
 const form = () => document.getElementById("find-cases")
 const allInputs = () => document.getElementById("all-inputs")
 
+
 document.addEventListener("DOMContentLoaded", function(){
  onCall()
  myApi()
@@ -17,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function(){
  blink2()
  myApi3()
  blink3()
+ getRails()
 
 })
 
@@ -179,69 +181,134 @@ console.log(value)
       function submit(e) {
       e.preventDefault()
     myApi4()
+    
       }
+      
 
       function search(value) {
+    
+        const baseUrl = 'http://localhost:3000'
         const searchValue = document.getElementById("countryName")
         const country = value.response.find(val => val.country == searchValue.value)
- 
-        
-const cName = country.country
-const cCont = country.continent
-const cCases = country.cases.total
-const cCasesR = country.cases.recovered
-const cCasesN = country.cases.new
-const cCasesA = country.cases.active
-const cNewD = country.deaths.new 
-const cTotalD = country.deaths.total
-const cPop = country.population 
-const cTest = country.tests.total
 
-debugger;
+const strongParams = {
+           search: { 
+        country: country.country,
+        continent: country.continent,
+        countryPopulation: country.population, 
+        totalCases: country.cases.total,
+        recovered: country.cases.recovered,
+        newCases: country.cases.new,
+        activeCases: country.cases.active,
+        newDeaths: country.deaths.new,
+        totalDeaths: country.deaths.total,
+        countryPopulation: country.population, 
+        totalTests: country.tests.total,
+        date: country.day
+}
+      }
+
+
 const ser = document.getElementById("searchR")
 
-const h4 = document.createElement("p")
-const h41 = document.createElement("p")
-const h42 = document.createElement("p")
-const h42 = document.createElement("p")
-const h42 = document.createElement("p")
-const h42 = document.createElement("p")
+const p = document.createElement("p")
+const p2 = document.createElement("p")
+const p3 = document.createElement("p")
+const p4 = document.createElement("p")
+const p5 = document.createElement("p")
+const p6 = document.createElement("p")
+const p7 = document.createElement("p")
+const p8 = document.createElement("p")
+const p9 = document.createElement("p")
+const p10 = document.createElement("p")
+const p11 = document.createElement("p")
 
-h4.innerText = country.country
-h41.innerText = country.continent
-h42.innerText = country.cases.total
+p.innerText = country.country
+p2.innerText = country.continent
+p3.innerText = country.cases.total
+p4.innerText = country.cases.recovered
+p5.innerText = country.cases.new
+p6.innerText = country.cases.active
+p7.innerText = country.deaths.new 
+p8.innerText = country.deaths.total
+p9.innerText = country.population 
+p10.innerText = country.tests.total
+p11.innerText = country.day
 
-ser.appendChild(h4)
-ser.appendChild(h41)
-ser.appendChild(h42)
+
+
+ser.appendChild(p)
+ser.appendChild(p2)
+ser.appendChild(p3)
+ser.appendChild(p4)
+ser.appendChild(p5)
+ser.appendChild(p6)
+ser.appendChild(p7)
+ser.appendChild(p8)
+ser.appendChild(p9)
+ser.appendChild(p10)
+
+ser.appendChild(p11)
+
+
+fetch(baseUrl + '/searches.json', {
+    method: "post",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(strongParams)
+  })
+    .then(resp => resp.json())
+    .then(country => { 
+    (country);
+    })
 
 resetInputs()
 
-resetForm()
     }
-
-    function resetInputs() {
+ 
+   function resetInputs() {
         const searchValue = document.getElementById("countryName")
         const ser = document.getElementById("searchR")
         searchValue.value = ""
+     }
 
-  
+      function getRails() {
+        const baseUrl = 'http://localhost:3000'
+
+        fetch(baseUrl + '/searches.json')
+        .then(resp => {
+          if (resp.status !== 200) {
+            throw new Error(resp.statusText);
+          }
+          return resp.json()
+        })
+        .catch(errors => console.log(errors))
+        .then(data => userInfo(data))
+     
+
       }
 
-      function resetForm() {
-          if(form().value ){
-                form().value = ""    
-              }
-              debugger;
+
+      function userInfo(data) {
+
+      const userCountry = data.map(da => da.country)
+
+      const userInfo = document.getElementById("userInfo")
+
+      const p = document.createElement("p")
+
+      p.innerText = userCountry
+     
+      userInfo.appendChild(p)
+
+
+
       }
 
+        
 
-  
-//       function formm() {
-//  const h2 = document.createElement("h2")
-//  h2.innerText = 
-// allInputs().appendChild(h2)
-//       }
 
 
 
